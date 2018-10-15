dbec0ea438188c57e7f05da26b72102e45eef106
mrdogan7 <mrdogan7@gmail.com>
2018-10-15 20:37:26 +0300

    asd

--- searching/merge.h
+++
@@ -1,6 +0,0 @@
-#ifndef MERGE_H
-#define MERGE_H
-
-void merge(int array[], int left, int middle, int right);
-
-#endif
--- searching/mergeSort.h
+++
@@ -1,6 +0,0 @@
-#ifndef MERGESORT_H
-#define MERGESORT_H
-
-void mergeSort(int array[], int left, int right);
-
-#endif
