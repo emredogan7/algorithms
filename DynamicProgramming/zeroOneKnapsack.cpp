f31661445d8bfc98bfd4d15a6360f267d7e5ff36
mrdogan7 <mrdogan7@gmail.com>
2019-01-20 09:18:48 +0300

    knapsack added

--- DynamicProgramming/zeroOneKnapsack.cpp
+++ DynamicProgramming/zeroOneKnapsack.cpp
@@ -1,8 +1,34 @@
+// dynamic programming solution to zero-one knapsack problem.
+// January 20, 2019
 // Emre Dogan
+
 
 int max(int num1,int num2) {return (num1 > num2)?num1 : num2;}
 
 
-int knapsackTable(int bagSize, int weights[], int numOfGoods)
+int knapsack(int bagSize, int weights[], int numOfGoods)
 {
-    // creating the table that is sized of (numOfGoods+1, bagSize+1).
+	// creating the table that is sized of (numOfGoods+1, bagSize+1) or (n+1,W+1).
+	int c[numOfGoods+1,bagSize+1];
+
+	for (int i = 0; i <= numOfGoods; i++)
+	{
+		for (int w = 0; w <= bagSize; w++)
+		{
+			if (i == 0 || w == 0)
+				c[i,w] = 0
+			else if ()
+
+			else
+
+		}
+	}
+}
+
+
+int main()
+{
+
+}
+
+
