96fe34e34a0106efbde7044266ffcfa1da9d54b2
mrdogan7 <mrdogan7@gmail.com>
2019-01-22 00:16:50 +0300

    Completed zeroOneKnapsack.cpp

--- dynamic programming/zeroOneKnapsack.cpp
+++ dynamic programming/zeroOneKnapsack.cpp
@@ -2,12 +2,12 @@
 // January 20, 2019
 // Emre Dogan
 
-#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
 
 int max(int num1,int num2) {return (num1 > num2)?num1 : num2;}
 
-
-int knapsack(int bagSize, int weights[],int values[], int numOfGoods)
+int knapsack(int numOfGoods, int bagSize, int weights[],int values[])
 {
 	// creating the table that is sized of (numOfGoods+1, bagSize+1) or (n+1,W+1).
 	int c[numOfGoods+1][bagSize+1];
@@ -22,7 +22,6 @@ int knapsack(int bagSize, int weights[],int values[], int numOfGoods)
 				c[i][w] = max(c[i-1][w], values[i] + c[i-1][w-weights[i]]);
 			else
 				c[i][w] = c[i-1][w];
-
 		}
 	}
 	return c[numOfGoods][bagSize];
@@ -30,11 +29,32 @@ int knapsack(int bagSize, int weights[],int values[], int numOfGoods)
 
 int main()
 {
-    int val[] = {60, 100, 120};
-    int wt[] = {10, 20, 30};
-    int  W = 50;
-    int n = sizeof(val)/sizeof(val[0]);
-    cout << knapsack(W, wt, val, n);
+	// Taking the number of elements to be taken.
+	int numOfGoods;
+	cout<<"\nEnter the number of possible elements to put into knapsack: ";
+	cin>>numOfGoods;
+
+	// Taking the capacity of your knapsack.
+	int bagSize;
+	cout<<"\nEnter the capacity of your knapsack: ";
+	cin>>bagSize;
+
+	// Taking the weights and values of these elements.
+	int weights[numOfGoods];
+	int values[numOfGoods];
+	cout<<"\nEnter  the weight and value of each item  seperated by space:\n";	
+	for(int i=0; i < numOfGoods; i++)
+	{
+		cin>>weights[i];
+		cin>>values[i];	
+	}
+	cout << "\nMaximum value of items in the knapsack is equal to " << knapsack(numOfGoods,bagSize,weights,values)<<'\n';
+
+    // int val[] = {60, 100, 120};
+    // int wt[] = {10, 20, 30};
+    // int  W = 50;
+    // int n = sizeof(val)/sizeof(val[0]);
+    // std :: cout << knapsack(W, wt, val, n);
     return 0;
 }
 
